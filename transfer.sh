0718414cfe59ab8b702b9be912dfa6b3f01c2ff1
Arijit <arijit.saha@zysk.tech>
2024-04-24 06:05:10 +0000

    Update: Shell script added to file transfer

--- transfer.sh
+++ transfer.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
+rm -rf ../chiratae-linkedIn-scrapper-backend/public/
 cp -ra ./build/. ../chiratae-linkedIn-scrapper-backend/public/
+touch ../chiratae-linkedIn-scrapper-backend/public/.gitignore
 echo "Build files copied to BE directory!!"
