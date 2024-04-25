d4381c927d4885a2d768c7ac14366210b7073f0f
Arijit <arijit.saha@zysk.tech>
2024-04-25 07:51:10 +0000

    Setup: codebase

--- transfer.sh
+++ transfer.sh
@@ -1,2 +1,2 @@
-cd .. && rm -rf ./chiratae-linkedIn-scrapper-backend/public && cp -r ./chiratae-linkedIn-scrapper-frontend/build ./chiratae-linkedIn-scrapper-backend/public && chmod -R 777 ./chiratae-linkedIn-scrapper-backend/public/
+cd .. && rm -rf ./chiratae-linkedIn-scrapper-backend/public && cp -r ./chiratae-linkedIn-scrapper-frontend/build ./chiratae-linkedIn-scrapper-backend/public
 echo "Build files copied to BE directory!!"
