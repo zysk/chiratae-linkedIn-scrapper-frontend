266cfb3ebcf857c9a30e8ecb54f3dce6a9e40eb6
Jahid MD <jahid.md@zysk.tech>
2024-07-17 17:39:03 +0530

    assets/images added

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -4,11 +4,11 @@ import React from "react";
 
 const Logo = () => {
   return (
-    <Link href="/" className="flex items-center space-x-2">
-      <div className="bg-primary-first h-8 w-8 rounded-full flex items-center justify-center">
-        <Image width={15.66} height={22.63} alt="logo" src="/union.svg" />
-      </div>
-      <h2 className="">Chiratae</h2>
+    <Link href="/" className="flex items-center space-x-2 justify-center">
+      {/* <div className="bg-primary-first h-8 w-8 rounded-full flex items-center justify-center"> */}
+        <Image width={100} height={100} alt="logo" src="/images/logo.png" />
+      {/* </div> */}
+      {/* <h2 className="">Chiratae</h2> */}
     </Link>
   );
 };
