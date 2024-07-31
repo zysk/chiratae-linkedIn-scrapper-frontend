4841cf8ed59a828c0360ad5582aa0d6f27244fee
Jahid MD <jahid.md@zysk.tech>
2024-07-31 11:37:33 +0530

    react hook form included with validations

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -6,6 +6,9 @@ import { addUser, updateUser } from '../../services/users.service';
 import { toastError, toastSuccess } from '../../utils/toastUtils';
 import CustomButton from '../Utility/Button';
 import { ConfirmModal } from '../Utility/ConfirmationModal';
+import { Controller, useForm } from "react-hook-form"
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup";
 
 const COMFIRMATION_DATA = {
     update_user: {
@@ -14,17 +17,14 @@ const COMFIRMATION_DATA = {
         title:"You can't revert changes"
     },
 }
+const emailPattern = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+const phonePattern = /^[0-9]{10,15}$/;
+
 
 export default function AddUsers({ selectedUser, setSelectedUser, setChange }) {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [phone, setPhone] = useState("");
-    const [employeeId, setEmployeeId] = useState("");
-    const [password, setPassword] = useState("");
-    const [isActive, setIsActive] = useState(false);
     const [confirmModal, setConfirmModal] = useState(false);
     const[confirmModalData,setConfirmModalData] = useState({})
-
+    
     const openConfirmModal = (data,row_id)=>{
         setConfirmModal(true);
         setConfirmModalData({...data,row_id});
@@ -42,46 +42,48 @@ export default function AddUsers({ selectedUser, setSelectedUser, setChange }) {
         }
     }
 
-    const handleAddCategory = async () => {
+	const schema = yup.object().shape({
+		name: yup.string()
+						.required("Name is Required")
+						.min(3, "Name should be at least 3 characters")
+						.max(50, "Name should not exceed 50 characters"),
+		email: yup.string()
+                        .matches(emailPattern, 'Invalid email format')
+                        .required('Email is required'),
+		phone: yup.string()
+                        .matches(phonePattern, 'Invalid phone number format')
+						.required("Phone is Required"),
+		employee_id: yup.string()
+						.required("Employee ID is Required"),
+		password: yup.string()
+						.required("Password is Required"),
+        is_active:yup.boolean()
+	});
+
+    const { control, handleSubmit, formState: { errors }, reset } = useForm({
+            resolver: yupResolver(schema),
+            defaultValues:{
+                name: "",
+                email: "",
+                phone: "",
+                employee_id: "",
+                password: "",
+                is_active:false
+            }
+        });
+
+    const handleAddCategory = async (data) => {
         try {
-            if (name == "") {
-                toastError("Name Field cannot be empty!");
-                return;
-            }
-            else if (email == "") {
-                toastError("Email Field cannot be empty!");
-                return;
-            }
-            else if (password == "" && !selectedUser && !selectedUser?.name) {
-                toastError("Password Field cannot be empty!");
-                return;
-            }
-            // else if (!email.includes("@")) {
-            //     toastError("Invalid email!");
-            //     return;
-            // }
-            // else if (!email.includes(".")) {
-            //     toastError("Invalid email!");
-            //     return;
-            // }
-            else if (phone == "") {
-                toastError("Phone number Field cannot be empty!");
-                return;
-            }
-            else if (employeeId == "") {
-                toastError("EmployeeId Field cannot be empty!");
-                return;
-            }
             let obj = {
-                name,
-                email,
-                phone,
-                employeeId,
-                isActive,
+                name: data.name,
+                email: data.email,
+                phone: data.phone,
+                employeeId: data.employee_id,
+                isActive: data.is_active,
                 role: "USER"
             };
-            if (password != "") {
-                obj.password = Buffer.from(password).toString("base64")
+            if (data.password) {
+                obj.password = Buffer.from(data.password).toString("base64")
             }
             if (selectedUser && selectedUser.name) {
                 openConfirmModal({...COMFIRMATION_DATA.update_user, user_data: obj })
@@ -92,14 +94,33 @@ export default function AddUsers({ selectedUser, setSelectedUser, setChange }) {
                     toastSuccess(res.message);
                     setChange(prev => prev + 1)
                 }
-                handleClearState()
+                reset({});
             }
         }
         catch (err) {
             toastError(err);
         }
-        // dispatch(CATEGORYAdd(obj));
+            // dispatch(CATEGORYAdd(obj));
+        }
+
+  
+    const onSubmitHandler = (data) => {
+        handleAddCategory(data);
     };
+
+    useEffect(() => {
+        if (selectedUser && selectedUser.name) {
+            reset({
+                name: selectedUser?.name,
+                email: selectedUser?.email,
+                phone: selectedUser?.phone,
+                employee_id: selectedUser?.employeeId,
+                // password: selectedUser?.password,
+                is_active:selectedUser?.isActive
+            })
+            // dispatch(CATEGORYGet());
+        }
+    }, [selectedUser,reset]);
 
     const handleUpdateUser = async (obj) => {
         let { data: res } = await updateUser(obj, selectedUser?._id);
@@ -107,76 +128,153 @@ export default function AddUsers({ selectedUser, setSelectedUser, setChange }) {
             toastSuccess(res.message);
             setSelectedUser({})
             setChange(prev => prev + 1)
-            handleClearState();
+            reset({
+                name: "",
+                email: "",
+                phone: "",
+                employee_id: "",
+                password: "",
+                is_active:false
+            })
         }
     };
-    
-    useEffect(() => {
-        if (selectedUser && selectedUser.name) {
-            setName(selectedUser?.name)
-            setEmail(selectedUser?.email)
-            setPhone(selectedUser?.phone)
-            setEmployeeId(selectedUser?.employeeId)
-            setIsActive(selectedUser?.isActive)
-            // setPassword(selectedUser?.password)
-            // dispatch(CATEGORYGet());
-        }
-    }, [selectedUser]);
-
-    const handleClearState = () => {
-        setName("")
-        setEmail("")
-        setPhone("")
-        setEmployeeId("")
-        setPassword("")
-    }
 
     return (
 
-
-
-
         <div className={"makeChange "}>
-            <form action="#" className="form row">
+            <form className="form row" onSubmit={handleSubmit(onSubmitHandler)}>
                 <div className={"col-6"}>
                     {/* <label className="blue-1 fs-12">
                         Name <span className="red">*</span>
                     </label> */}
-                    <TextField sx={{ borderRadius: 5 }} id="outlined-basic" label="Name" value={name} onChange={(event) => setName(event.target.value)} type="text" className="form-control" />
+                    <Controller
+                      name="name"
+                      control={control}
+                      render={({ field }) => (
+                            <TextField 
+                                sx={{ borderRadius: 5 }} 
+                                id="outlined-basic" 
+                                label="Name" 
+                                value={field.value} 
+                                onChange={(e) => field.onChange(e.target.value)}
+                                type="text" 
+                                className="form-control"
+                            />
+                        )
+                    }
+                    />
+                    {errors.name && <div className="text-danger mt-1">{errors.name.message}</div>}
                 </div>
                 <div className={"col-6"}>
                     {/* <label className="blue-1 fs-12">
                         Email <span className="red">*</span>
                     </label> */}
-                    <TextField sx={{ borderRadius: 5 }} id="outlined-basic" label="Email" value={email} onChange={(event) => setEmail(event.target.value)} type="email" className="form-control" />
+                    <Controller
+                      name="email"
+                      control={control}
+                      render={({ field }) => (
+                            <TextField 
+                                sx={{ borderRadius: 5 }} 
+                                id="outlined-basic" 
+                                label="Email" 
+                                value={field.value} 
+                                onChange={(e) => field.onChange(e.target.value)}
+                                type="text" 
+                                className="form-control"
+                            />
+                        )
+                    }
+                    />
+                    {errors.email && <div className="text-danger mt-1">{errors.email.message}</div>}
                 </div>
                 <div className={"col-6"}>
                     {/* <label className="blue-1 fs-12">
                         Phone Number <span className="red">*</span>
                     </label> */}
-                    <TextField sx={{ borderRadius: 5 }} id="outlined-basic" label="Phone Number" value={phone} onChange={(event) => setPhone(event.target.value)} type="number" className="form-control" />
+                    <Controller
+                      name="phone"
+                      control={control}
+                      render={({ field }) => (
+                            <TextField 
+                                sx={{ borderRadius: 5 }} 
+                                id="outlined-basic" 
+                                label="Phone Number" 
+                                value={field.value} 
+                                onChange={(e) => field.onChange(e.target.value)}
+                                type="text" 
+                                className="form-control"
+                            />
+                        )
+                    }
+                    />
+                    {errors.phone && <div className="text-danger mt-1">{errors.phone.message}</div>}
+                        
                 </div>
                 <div className={"col-6"}>
                     {/* <label className="blue-1 fs-12">
                         Employee Id <span className="red">*</span>
                     </label> */}
-                    <TextField sx={{ borderRadius: 5 }} id="outlined-basic" label=" Employee Id" value={employeeId} onChange={(event) => setEmployeeId(event.target.value)} type="text" className="form-control" />
+                    <Controller
+                      name="employee_id"
+                      control={control}
+                      render={({ field }) => (
+                            <TextField 
+                                sx={{ borderRadius: 5 }} 
+                                id="outlined-basic" 
+                                label="Employee Id" 
+                                value={field.value} 
+                                onChange={(e) => field.onChange(e.target.value)}
+                                type="text" 
+                                className="form-control"
+                            />
+                        )
+                    }
+                    />
+                    {errors.employee_id && <div className="text-danger mt-1">{errors.employee_id.message}</div>}
                 </div>
                 <div className={"col-6"}>
                     {/* <label className="blue-1 fs-12">
                         Password <span className="red">*</span>
                     </label> */}
-                    <TextField sx={{ borderRadius: 5 }} id="outlined-basic" label="Password" value={password} onChange={(event) => setPassword(event.target.value)} type="password" className="form-control" />
+                    <Controller
+                      name="password"
+                      control={control}
+                      render={({ field }) => (
+                            <TextField 
+                                sx={{ borderRadius: 5 }} 
+                                id="outlined-basic" 
+                                label="Password" 
+                                value={field.value} 
+                                onChange={(e) => field.onChange(e.target.value)}
+                                type="text" 
+                                className="form-control"
+                            />
+                        )
+                    }
+                    />
+                    {errors.password && <div className="text-danger mt-1">{errors.password.message}</div>}
                 </div>
                 <div className='row'>
                     <div className={"col-6"}>
-                        <input checked={isActive} onChange={(event) => setIsActive(event.target.checked)} type="checkbox" />
-                        <label className="blue-1 ms-2 fs-12">
-                            Is Active
-                        </label>
+                    <Controller
+                      name="is_active"
+                      control={control}
+                      render={({ field }) => (
+                        <>
+                            <input 
+                                checked={field.value} 
+                                onChange={(e) =>field.onChange(e.target.checked)} type="checkbox" />
+                            <label className="blue-1 ms-2 fs-12">
+                                Is Active
+                            </label>
+                        </>
+
+                        )
+                    }
+                    />
                     </div>
                     <div className="col-6 text-end">
-                        <CustomButton btntype="button" changeClass="me-4 btn btn-1 btn-outline" ClickEvent={handleAddCategory} iconName="fa-solid fa-check" btnName="Save " isBtn small={false} />
+                        <CustomButton btntype="submit" changeClass="me-4 btn btn-1 btn-outline" iconName="fa-solid fa-check" btnName="Save " isBtn small={false} />
                     </div>
                 </div>
 
