8fed6ab8f5576ac9fafde3d4e0f27323e6ab2a3a
themanvendra08 <manvendra.singh@zysk.tech>
2024-09-30 14:00:21 +0530

    fox: added deployed link

--- src/services/url.service.js
+++ src/services/url.service.js
@@ -1,5 +1,5 @@
 ////local url
-export const url = "http://139.59.7.190:4051";
+// export const url = "http://139.59.7.190:4051";
 // export const url = "http://127.0.0.1:4051";
 // export const url = "http://192.168.0.34:4051";
 // export const url = "http://192.168.0.16:4050";
@@ -8,3 +8,6 @@ export const url = "http://139.59.7.190:4051";
 // export const url = "http://3.111.44.119/api"
 // export const url = "/api";
 // export const url = "https://linkedinscrapper.chiratae.com/api";
+
+// HOSTED URL
+export const url = "https://linkedinscrapper.zysk.in"
