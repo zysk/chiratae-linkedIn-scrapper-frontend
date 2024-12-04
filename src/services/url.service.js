24556e4cb741475199b52b56282f7f1751bbbcf3
themanvendra08 <manvendra.singh@zysk.tech>
2024-12-04 17:09:10 +0530

    fix: update the base url

--- src/services/url.service.js
+++ src/services/url.service.js
@@ -1,13 +1,1 @@
-////local url
-// export const url = "http://139.59.7.190:4051";
-// export const url = "http://127.0.0.1:4051";
-// export const url = "http://192.168.0.34:4051";
-// export const url = "http://192.168.0.16:4050";
-/////live url
-// export const url = "http://63.33.52.10:3002/api"
-// export const url = "http://3.111.44.119/api"
-// export const url = "/api";
-// export const url = "https://linkedinscrapper.chiratae.com/api";
-
-// HOSTED URL
-export const url = "https://linkedinscrapper.zysk.in"
+export const url = "https://linkedinscrapper.chiratae.com";
